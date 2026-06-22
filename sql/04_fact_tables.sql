29e3149002cd99e9c961c0f133b298dd7c36c1e8
Karthik N <knk2183@gmail.com>
2026-06-22 17:56:59 +0530

    fact table updated

--- sql/04_fact_tables.sql
+++ sql/04_fact_tables.sql
@@ -6,8 +6,9 @@ DROP TABLE IF EXISTS fact_employee_attrition;
 CREATE TABLE fact_employee_attrition AS
 select
 	employeenumber,
+	department,
+	jobrole,
 	attrition,
-	age,
 	monthlyincome,
 	totalworkingyears,
 	yearsatcompany,
@@ -27,6 +28,21 @@ FOREIGN KEY (employeenumber)
 REFERENCES dim_employee(employeenumber);
 
 
+ALTER TABLE fact_employee_attrition
+ADD CONSTRAINT fk_fact_department
+FOREIGN KEY (department)
+REFERENCES dim_department(department);
+
+
+ALTER TABLE fact_employee_attrition
+ADD CONSTRAINT fk_fact_jobrole
+FOREIGN KEY (jobrole)
+REFERENCES dim_jobrole(jobrole);
+
+SELECT conname
+FROM pg_constraint
+WHERE conrelid = 'fact_employee_attrition'::regclass;
+
 SELECT employeenumber,
        COUNT(*)
 FROM fact_employee_attrition
@@ -36,3 +52,13 @@ HAVING COUNT(*) > 1;
 
 alter table fact_employee_attrition
 drop column age;
+
+SELECT *
+FROM fact_employee_attrition
+LIMIT 5;
+
+ALTER TABLE fact_employee_attrition
+DROP CONSTRAINT fk_fact_employee;
+
+select count(*)
+from fact_employee_attrition fea ;
